b3f5d771be7fd94284aaaf8de58a11f994f92550
suumitru01 <telmore.sumit@gmail.com>
2023-07-03 16:24:57 +0530

    Added New Scenario to the Login Page

--- features/loginPage.feature
+++ features/loginPage.feature
@@ -2,4 +2,9 @@ Feature: Login Fetaure for OrangeHRMLive
   Scenario: 1. Test login functionality with in-valid credentials
     Given I launch the browser
     When I navigate to the url
+    Then I enter username as Sumit and password as admin
+    And I click on Login button
+    And I validate error message as Invalid credentials on home screen
+    And I close the browser
+
 
--- features/steps/loginPage.py
+++ features/steps/loginPage.py
@@ -1,7 +1,10 @@
+import time
+
 from selenium import webdriver
-from behave import given, when
+from behave import given, when, then, step
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 
 @given('I launch the browser')
@@ -20,3 +23,26 @@ def navigate_to_url(context):
     context.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     actual_titleName = context.driver.title
     assert expected_titleName == actual_titleName
+
+
+@then('I enter username as {user_name} and password as {pass_word}')
+def enter_username(context, user_name, pass_word):
+    time.sleep(3)
+    context.driver.find_element(By.CSS_SELECTOR, 'input[name="username"]').send_keys(user_name)
+    context.driver.find_element(By.CSS_SELECTOR, 'input[name="password"]').send_keys(pass_word)
+
+
+@step('I click on Login button')
+def click_on_login_button(context):
+    context.driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+
+
+@step('I validate error message as {expected_message} on home screen')
+def validate_invalid_error_msg(context, expected_message):
+    time.sleep(3)
+    actual_message = context.driver.find_element(By.CSS_SELECTOR, 'p[class="oxd-text oxd-text--p oxd-alert-content-text"]')
+    assert expected_message == actual_message.text
+
+@step('I close the browser')
+def close_browser(context):
+    context.driver.quit()
